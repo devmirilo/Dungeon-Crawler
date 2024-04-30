48f4f594499afe973fcea0d4875b01b84e7f9cdd
Murilo Pantoja Carneiro <158524340+devmirilo@users.noreply.github.com>
2024-04-30 20:46:55 -0300

    commit 3

--- main.c
+++ main.c
@@ -2,15 +2,14 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(int argc, char *argv[]) {
-	int menu1;
-	int i, j, k;
-	int vida = 3;
-	printf("1) Jogar\n2) Tutorial\n3) Sair\n");
-	scanf("%d", &menu1);
-	if(menu1 == 1){
-		system("cls");
+void tutorial(){
+	printf("Ola, Bravo Aventureiro, Hoje voce ira passar por um nova aventura. O jogo consiste em pegar a chave '@', e passar pelas portas 'D', caso a porta seja um '=', quer dizer que a porta esta aberta, podendo passar sem a chave!, a movimentacao consiste em usar 'w' 'a' 's' 'd', e pressione 'q' para sair ");
+	getch();
+	system("cls");
+}
+void jogar(){
+	int i, j;
+	system("cls");
 		int x = 8, y = 8;
 		char mapa1[10][10];
 		for(i = 0; i < 10; i++){
@@ -43,10 +42,12 @@ int main(int argc, char *argv[]) {
 		}
 		mapa1[1][3] = 'D';
 		mapa1[2][1] = '@';
-		mapa1[6][2] = '=';
+		mapa1[6][2] = 'D';
 		mapa1[1][8] = '@';
 		mapa1[3][7] = '=';
 		mapa1[y][x] = '&';
+		mapa1[9][1] = 'D';
+		mapa1[8][2] = '@';
 		while(1){
 			for(i = 0; i < 10; i ++){
 				for(j = 0; j < 10; j ++){
@@ -57,38 +58,62 @@ int main(int argc, char *argv[]) {
 			int ch = getch();
 			
 			if(ch == 'w'){
-				if(x > 1) x--;
-				mapa1[x+1][y] = ' ';
+				if(mapa1[x-1][y] != '*'){
+				 x--;
+				mapa1[x+1][y] = ' ';	
+				}
 				system("cls");
 			}
 			else if(ch == 's'){
-				if(x < 8) x++;
+				if(mapa1[x+1][y] != '*'){
+				x++;
 				mapa1[x-1][y] = ' ';
+				}
 				system("cls");	
 			}
 			else if(ch == 'a'){
-				if (y > 1) y--;
+				if (mapa1[x][y-1] != '*'){
+				y--;
 				mapa1[x][y+1] = ' ';
+				}
 				system("cls");
 			}
 			else if(ch == 'd'){
-				if(y < 8) y++;
+				if(mapa1[x][y+1] != '*'){
+				y++;
 				mapa1[x][y-1] = ' ';
+				}
 				system("cls");
 			}
 			else if(ch == 'q'){
-				break;
+				system("cls");
+				printf("Voce saiu.");
+				getch();
+				menu();
 			}
 			mapa1[x][y] = '&';
 		}
 	}
-	else if(menu1 == 2){
-		printf("oi, como vai? hoje iremos te ensinar a como ");
+void sair(){
+	printf("Voce saiu. Por favor volte novamente.");
+	getch();
+	return 0;
+}
+void menu(){
+	int menu1;
+	printf("1) Jogar\n2) Tutorial\n3) Sair\n");
+	scanf("%d", &menu1);
+	if(menu1 == 1){
+		jogar();
 	}
-	else if(menu1 == 3){
-		system("cls");
-		printf("Voce saiu, Volte depois.");
+	else if(menu1 == 2){
+		tutorial();
+		menu();
+	}
+	else{
 		return 0;
 	}
-	return 0;
+}
+int main(int argc, char *argv[]) {
+	menu();
 }
