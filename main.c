a20cd3c0effc789d8652489ec479cd24c6697c4c
Murilo Pantoja Carneiro <158524340+devmirilo@users.noreply.github.com>
2024-04-30 22:30:46 -0300

    commit 5

--- main.c
+++ main.c
@@ -1,41 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+//Voids
+void tutorial();
+void jogar();
+void sair();
+
+//Menu Inicial
 void menu(){
 	int menu1;
-	printf("1) Jogar\n2) Tutorial\n3) Sair\n");
-	scanf("%d", &menu1);
-	if(menu1 == 1){
-		jogar();
-	}
-	if(menu1 == 2){
-		tutorial();
-		menu();
-	}
-	if(menu1 == 3){
-		sair();
-	}
+	do{
+		printf("1) Jogar\n2) Tutorial\n3) Sair\n");
+		scanf("%d", &menu1);
+		if(menu1 == 1){
+			jogar();
+		}
+		else if(menu1 == 2){
+			tutorial();
+		}
+		else if(menu1 == 3){
+			sair();
+			return;
+		}
+		else{
+			printf("Opcao invalida. Tente novamente!\n");
+		}
+	}while(menu1 != 3);
 }
+//Tutorial
 void tutorial(){
-	printf("Ola, Bravo Aventureiro, Hoje voce ira passar por um nova aventura. O jogo consiste em pegar a chave '@', e passar pelas portas 'D', caso a porta seja um '=', quer dizer que a porta esta aberta, podendo passar sem a chave!, a movimentacao consiste em usar 'w' 'a' 's' 'd', e pressione 'q' para sair ");
+	printf("Ola, Bravo Aventureiro, Hoje voce ira passar por um nova aventura, que consiste em 3 fases.\nO jogo consiste em pegar a chave '@', e passar pelas portas 'D' e '=', caso a porta seja um '=', quer dizer que a porta esta aberta. A movimentacao consiste em usar 'w' 'a' 's' 'd' e pressionar 'q' para sair.\n");
+	printf("Pressione qualquer tecla para voltar ao menu.\n");
 	getch();
 	system("cls");
 }
+//Jogo
 void jogar(){
 	int i, j;
 	system("cls");
-		int x = 8, y = 8;
+		//Criar o Mapa
 		char mapa1[10][10];
 		for(i = 0; i < 10; i++){
 			for(j = 0; j < 10; j++){
+				//Paredes externas
 				if(i == 0 || i == 9){
 					mapa1[i][j] = '*';
 				}
 				else if (j == 0 || j == 9){
 					mapa1[i][j] = '*';
 				}
-				else if (i == 3 && (j > 0 && j < 4) || i == 3 && (j > 5 && i < 10)){
+				//Parede interna
+				else if ((i == 3 && (j > 0 && j < 4)) || (i == 3 && (j > 5 && i < 10))){
 					mapa1[i][j] = '*';
 				}
 				else if (i == 6 && (j > 0 && j < 4)){
@@ -50,11 +66,16 @@ void jogar(){
 				else if(j == 6 && i < 4){
 					mapa1[i][j] = '*';
 				}
+				//Espacos Vazios
 				else if (i > 0 && i < 9){
 					mapa1[i][j] = ' ';
 				}
 			}
 		}
+		//Posição inicial do jogador
+		int x = 8, y = 8;
+		//Colocar os elementos no mapa
+		char chave1, chave2, chave3, porta1, porta2, porta3;
 		mapa1[1][3] = 'D';
 		mapa1[2][1] = '@';
 		mapa1[6][2] = 'D';
@@ -63,6 +84,12 @@ void jogar(){
 		mapa1[y][x] = '&';
 		mapa1[9][1] = 'D';
 		mapa1[8][2] = '@';
+		chave1 = mapa1[1][8];
+		chave2 = mapa1[2][1];
+		chave3 = mapa1[8][2];
+		porta1 = mapa1[1][3];
+		porta2 = mapa1[6][2];
+		porta3 = mapa1[9][1];
 		while(1){
 			for(i = 0; i < 10; i ++){
 				for(j = 0; j < 10; j ++){
@@ -70,8 +97,9 @@ void jogar(){
 				}
 				printf("\n");
 			}
+			printf("\n\nPressione 'q' para sair.\n");
 			int ch = getch();
-			
+			//Movimentação do jogador
 			if(ch == 'w'){
 				if(mapa1[x-1][y] != '*'){
 				 x--;
@@ -101,21 +129,25 @@ void jogar(){
 				system("cls");
 			}
 			else if(ch == 'q'){
-				system("cls");
-				printf("Voce saiu.");
-				getch();
-				system("cls");
-				menu();
+				printf("Deseja sair do jogo? (S/N): ");
+        		char resposta;
+        		scanf(" %c", &resposta);
+        		if(resposta == 'S' || resposta == 's') {
+            		printf("Voce saiu do jogo.\n");
+            		printf("Pressione qualquer tecla para voltar ao menu.\n");
+            		getch();
+            		system("cls");
+            		return;
+				}
 			}
-			mapa1[x][y] = '&';
+				mapa1[x][y] = '&';
 		}
 	}
 void sair(){
-	printf("Voce saiu. Por favor volte novamente.\n");
-	getch();
-	return 0;
+	printf("Voce saiu. Por favor volte novamente outra hora.\n");
+	exit(0);
 }
-int main(int argc, char *argv[]) {
+int main() {
 	menu();
 	return 0;
 }
